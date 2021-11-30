6774815baaf35a32d1b3b70e863402b929dbb109
ANIMESH SRIVASTAVA <animeshsrivastava246246@gmail.com>
2021-11-30 07:26:27 +0530

    74 done

---
+++ src/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+    return (
+        <div className="my-3">
+            <h3>But Nike Shoes 100 Rs.</h3>
+            <button className="btn btn-primary mx-2">-</button>
+            Add to Cart
+            <button className="btn btn-primary mx-2">+</button>
+        </div>
+    )
+}
+
+export default Shop
